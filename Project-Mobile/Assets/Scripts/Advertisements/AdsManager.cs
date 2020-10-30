acac4f72c3efe0b305dd9269c4dc4f1fec9688fd
FrancescoLenolli <francescopietrolenolli@gmail.com>
2020-10-30 16:50:55 +0100

    First Setup for Ads

---
+++ Project-Mobile/Assets/Scripts/Advertisements/AdsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        Advertisement.Initialize("3884039", true);
+
+        while (!Advertisement.IsReady())
+        {
+            yield return null;
+        }
+
+        Advertisement.Show(); // Neat.
+    }
+}
