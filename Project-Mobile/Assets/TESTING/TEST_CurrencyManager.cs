15688991cc836db66876647364051cb074965efd
FrancescoLenolli <francescopietrolenolli@gmail.com>
2021-02-05 17:20:41 +0100

    Testing new game logic
    Current logic is not good at all.

---
+++ Project-Mobile/Assets/TESTING/TEST_CurrencyManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_CurrencyManager : MonoBehaviour
+{
+    private bool canTest = false;
+
+    public double currency;
+    public double premiumCurrency;
+    public double activeCurrencyGain;
+    public List<TEST_Ship> ships = new List<TEST_Ship>();
+
+    private void Start()
+    {
+        AddPassiveCurrency();
+    }
+
+    public void Update()
+    {
+        if (IsPlayerTapping())
+        {
+            canTest = true;
+        }
+    }
+
+    private void AddPassiveCurrency()
+    {
+        StartCoroutine(PassiveCurrencyGain());
+    }
+
+    private bool IsPlayerTapping()
+    {
+        return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetMouseButtonDown(0);
+    }
+
+    private double GetTotalPassiveCurrencyGain()
+    {
+        double currencyGain = 0f;
+        foreach(TEST_Ship ship in ships)
+        {
+            currencyGain += ship.GetTotalCurrencyGain();
+        }
+        return currencyGain;
+    }
+
+    private IEnumerator PassiveCurrencyGain()
+    {
+        while(true)
+        {
+            if (canTest)
+            {
+                yield return new WaitForSeconds(1f);
+                currency += GetTotalPassiveCurrencyGain();
+                Debug.Log(currency.ToString());
+                canTest = false;
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Project-Mobile/Assets/TESTING/TEST_Ship.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_Ship : MonoBehaviour
+{
+    private double totalCurrencyGain;
+
+    public double currencyGain;
+    public int quantity;
+    public List<float> upgradeValues;
+
+    public TEST_Ship(double currencyGain, int quantity, List<float> upgradeValues)
+    {
+        this.currencyGain = currencyGain;
+        this.quantity = quantity;
+        this.upgradeValues = upgradeValues;
+    }
+
+    private void Start()
+    {
+        SetTotalCurrencyGain();    
+    }
+
+    public void Buy()
+    {
+        ++quantity;
+        SetTotalCurrencyGain();
+    }
+
+    public double GetTotalCurrencyGain()
+    {
+        return totalCurrencyGain;
+    }
+
+    private void SetTotalCurrencyGain()
+    {
+        float totalUpgrades = 0f;
+        foreach(float upgrade in upgradeValues)
+        {
+            totalUpgrades += upgrade;
+        }
+
+        double newCurrencyGain = totalUpgrades == 0f ? currencyGain : currencyGain + (currencyGain * totalUpgrades / 100);
+
+        totalCurrencyGain = newCurrencyGain * quantity;
+
+        //Debug.Log($"{name} currencyGain\n\n" +
+        //    $"CurrencyGain: {currencyGain}\nUpgrade percentage: {totalUpgrades}\n" +
+        //    $"New CurrencyGain: {currencyGain} + {currencyGain * totalUpgrades / 100} = {currencyGain + (currencyGain * totalUpgrades / 100)}\n" +
+        //    $"TotalCurrencyGain: {newCurrencyGain} * {quantity} = {newCurrencyGain * quantity}\n");
+    }
+}
