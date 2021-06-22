b8e5533207adeaaa2261cb29c95d397677f1764d
FrancescoLenolli <francescopietrolenolli@gmail.com>
2021-06-22 14:45:17 +0200

    Fix to CameraBehaviour
    simplified direction's calculation

--- Project-Mobile/Assets/Scripts/GameElements/CameraBehaviour.cs
+++ Project-Mobile/Assets/Scripts/GameElements/CameraBehaviour.cs
@@ -3,6 +3,7 @@
 public class CameraBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform targetLookAt = null;
+    [SerializeField] private float positionOffset = 2.0f;
 
     private Vector3 originalPosition;
     private Vector3 direction;
@@ -12,7 +13,7 @@ public class CameraBehaviour : MonoBehaviour
         originalPosition = transform.position;
 
         // direction pointing at the camera's back
-        direction = transform.position - transform.forward;
+        direction = -transform.forward;
     }
 
     private void LateUpdate()
@@ -22,6 +23,6 @@ public class CameraBehaviour : MonoBehaviour
 
     public void ChangePosition(int offset)
     {
-        transform.position = originalPosition + (direction * (offset * 0.5f));
+        transform.position = originalPosition + (direction * (offset * positionOffset));
     }
 }
